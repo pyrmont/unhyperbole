94d736c53f0b6af5e14921f318e1ee29becf07e1
pyrmont <mike@inqk.net>
2011-09-27 11:48:23 +0900

    Display the post titles.

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require 'sinatra'
 require 'feedzirra'
 
 get '/' do
+  html = ''
+
   feed = Feedzirra::Feed.fetch_and_parse("http://techcrunch.com/author/mg-siegler/feed/")
   feed.title
+  
+  feed.entries.each do |entry|
+    html += entry.title
+    html += '<br/>'
+  end
+  
+  html
 end
